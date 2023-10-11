56a21479d26a25c83fed340734057aa8365e6327
AbarnaRagu <131658876+AbarnaRagu@users.noreply.github.com>
2023-10-11 22:28:14 +0530

    Program6

---
+++ Vowel.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    char c;
+    printf("\n enter the character");
+    scanf("%c",&c);
+    if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+    printf("%c is vowel",c);
+        else
+     printf("\n%c is not a vowel",c);
+     return 0;
+}
