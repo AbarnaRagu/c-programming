8be44e45bcb336570277cc0ecfeac88cbc0409dc
AbarnaRagu <131658876+AbarnaRagu@users.noreply.github.com>
2023-10-21 00:34:10 +0530

    Create calloc.c

---
+++ calloc.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n,i,*ptr;
+    printf("\n enter the no of values:");
+    scanf("%d",&n);
+    ptr=(int*)calloc(n,sizeof(int));
+   printf("\n enter the values:");
+    for(i=0;i<n;i++)
+    {
+    scanf("%d",ptr+i);
+    }
+
+printf("\n the values are:");
+for(i=0;i<n;i++)
+{
+    printf("\n%d",*(ptr+i));
+}   
+
+free(ptr);
+    return 0;
+}
+
